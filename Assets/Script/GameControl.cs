f07b53aead5d1723ce58e255b80a1780c71247d1
Awijie138 <mutawallysyarawy@gmail.com>
2020-08-24 19:17:18 +0800

    First Commit

---
+++ Assets/Script/GameControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+
+	public static GameControl instance;         
+    public Text scoreText;                      
+    public GameObject gameOvertext;             
+
+    private int score = 0;                      
+    public bool gameOver = false;              
+    public float scrollSpeed = -1.5f;
+	
+	
+	void Awake () {
+		
+        if (instance == null){
+            instance = this;
+		}
+        else if(instance != this){
+            Destroy (gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (gameOver && Input.GetMouseButtonDown(0) ) 
+        {
+            
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+	}
+	
+	public void DragonScored(){
+		if(gameOver){
+			return;
+		}
+		score++;
+		scoreText.text="Score: " + score.ToString();
+	} 
+	
+	public void DragonDied()
+    {
+        gameOvertext.SetActive (true);
+        gameOver = true;
+    }
+}
---
+++ Assets/Script/RepeatingBackground.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour {
+
+	private BoxCollider2D groundCollider;       
+    private float groundHorizontalLength;       
+
+ 
+    private void Awake ()
+    {
+       
+      
+    
+        groundHorizontalLength = 23.29f;
+    }
+
+    
+    private void Update()
+    {
+        
+        if (transform.position.x < -groundHorizontalLength)
+        {
+           
+            RepositionBackground ();
+        }
+    }
+
+    
+    private void RepositionBackground()
+    {
+        
+        Vector2 groundOffSet = new Vector2(groundHorizontalLength * 2f, 0);
+		
+        transform.position = (Vector2) transform.position + groundOffSet;
+    }
+}
